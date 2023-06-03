71fc71a76b107dfa5baad22721fe738d885427aa
keke <liutongke@live.com>
2023-06-03 14:59:16 +0800

    更新

--- monitor_nvr/start.bat
+++ monitor_nvr/start.bat
@@ -14,4 +14,3 @@ if exist "%data_dir%" (
 
 docker build -t monitor_nvr_go:v1 .
 docker run --name monitor_nvr_go_v1 -itd  -p 12349:12349 -p 9090:9090/udp -v %data_dir%:/var/www/html/data monitor_nvr_go:v1
-rem docker run --name go-v1 -itd  -p 9090:9090/udp -p 12346:12346 -v C:\Users\keke\dev\Raspberry-Pi\gpio:/var/www/html go-ffmpeg:latest /bin/bash
